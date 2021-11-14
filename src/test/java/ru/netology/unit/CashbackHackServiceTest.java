8597e558a0e33d0e89cc34d921acc3b3b69f4855
leonidbeltsov <leonidbeltsov@gmail.com>
2021-11-14 23:35:16 +0300

    junit4-platfotm

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,5 +1,8 @@
 package ru.netology.unit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -48,6 +51,50 @@ public class CashbackHackServiceTest {
         int expected = 999;
 
         assertEquals(actual, expected);
+    }
+
+//    JUnit Jupiter test
+
+    @Test
+    public void shouldShowActual100WithJUnitJupiter() {
+        amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowActual1WithJUnitJupiter() {
+        amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldShowActual0WithJUnitJupiter() {
+        amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+//        assertEquals(actual, expected);
+// Чтобы все тесты проходили и CI не падал, мы применим тут другой assert...
+        Assertions.assertTrue(actual != expected);
+    }
+
+    @Test
+    public void shouldShowActual999WithJUnitJupiter() {
+        amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        Assertions.assertEquals(actual, expected);
     }
 
 }
